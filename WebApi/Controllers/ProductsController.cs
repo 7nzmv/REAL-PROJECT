fc938aa1c628d3bf8bac87356cfe0a1b885a35cc
Umar <umarnizomov17@gmail.com>
2025-07-18 16:51:39 +0500

    add upload image

--- WebApi/Controllers/ProductsController.cs
+++ WebApi/Controllers/ProductsController.cs
@@ -11,7 +11,7 @@ namespace WebApi.Controllers;
 
 [ApiController]
 [Route("api/[controller]")]
-public class ProductsController(IProductService productService) : ControllerBase
+public class ProductsController(IProductService productService, IWebHostEnvironment environment) : ControllerBase
 {
 
     [HttpGet]
@@ -56,17 +56,27 @@ public class ProductsController(IProductService productService) : ControllerBase
         if (file == null || file.Length == 0)
             return BadRequest("Файл не выбран");
 
-        var filePath = Path.Combine("wwwroot/uploads", file.FileName);
+        // получаем путь к wwwroot/uploads
+        var uploadsFolder = Path.Combine(environment.WebRootPath, "uploads");
 
-        Directory.CreateDirectory("wwwroot/uploads"); // если директории нет
+        // создаём папку, если её нет
+        Directory.CreateDirectory(uploadsFolder);
+
+        // формируем безопасное имя файла (убираем пробелы и запрещённые символы)
+        var fileName = Path.GetFileName(file.FileName).Replace(" ", "_");
+
+        var filePath = Path.Combine(uploadsFolder, fileName);
+
+        // сохраняем файл
         using (var stream = new FileStream(filePath, FileMode.Create))
         {
             await file.CopyToAsync(stream);
         }
 
-        var imageUrl = $"{Request.Scheme}://{Request.Host}/uploads/{file.FileName}";
+        // формируем url для доступа к файлу
+        var imageUrl = $"{Request.Scheme}://{Request.Host}/uploads/{fileName}";
 
-        return Ok(new { Url = imageUrl });
+        return Ok(new { url = imageUrl });
     }
 
 }
